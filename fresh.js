a8d0919df172cb7d698967331c0eb4bc56b2c1d2
Nikhil <NikhilC2209@gmail.com>
2020-06-09 12:03:02 +0530

    Added a progress bar

--- fresh.js
+++ fresh.js
@@ -1,9 +1,13 @@
 const input = document.querySelector(".add");
 const add = document.querySelector(".btn");
 const list = document.querySelector(".list");
+var progress = document.querySelector(".bar");
+const percent = document.querySelector(".percent");
 load();
+bar();
 var collection1 = document.getElementsByClassName("g");
 var collection2 = document.getElementsByClassName("r");
+var inner = document.querySelector(".inner");
 var refresh = document.querySelector(".refresh");
 var check = Array.from(collection1);
 var del = Array.from(collection2);
@@ -71,6 +75,7 @@ function rem(item) {
 		localStorage.setItem("Completed", JSON.stringify(array2));
 		localStorage.setItem("UnCompleted", JSON.stringify(array3));
 		par.remove();
+		bar();
 	});		
 };
 
@@ -97,6 +102,7 @@ function load(item) {
 	}
 }
 
+
 function duplicate(array) {
 	var set = new Set(array);
 	return array = [...set]
@@ -121,6 +127,8 @@ function green(item) {
 			}
 		}
 	}
+	change(arr1,arr2);
+	reset_animation();
 localStorage.setItem("Completed", JSON.stringify(arr1));	
 localStorage.setItem("UnCompleted", JSON.stringify(arr2));	
 }
@@ -142,6 +150,8 @@ function create(item) {
 		for (i=0;i<comp.length;i++) {
 			if (item == comp[i]) {
 				color = "fas";
+				newDiv.style.textDecoration = "line-through";
+				newDiv.style.opacity = 0.4;
 				break
 			}
 		}
@@ -168,3 +178,48 @@ add.addEventListener("click", function() {
 	tick();
 	input.value = "";
 });
+
+//Progress Bar
+
+function bar() {
+	let total = JSON.parse(localStorage.getItem("List"));
+	let comp = JSON.parse(localStorage.getItem("Completed"));
+	
+	if (total !== null) {
+		len1 = total.length;
+	}
+	else {
+		len1 = 0;
+	}
+	if (comp !== null) {
+		len2 = comp.length;
+	}
+	else {
+		len2 = 1;
+	}
+
+	bar_length = len2/len1;
+
+	document.documentElement.style.setProperty('--length', bar_length*40 + "vw");
+	percent.innerText = Math.round(bar_length*100) + "%";
+}
+
+
+function change(array1, array2) {
+	
+	let len1 = array1.length;
+	let len2 = array2.length;		
+
+	bar_length = (len1/(len2+len1))*40;
+
+	document.documentElement.style.setProperty('--length', bar_length + "vw");
+	let value = (len1/(len2+len1))*100;
+	percent.innerText = Math.round(value) + "%";
+}
+
+function reset_animation() {
+  var el = document.querySelector('.inner');
+  el.style.animation = 'none';
+  el.offsetHeight; /* trigger reflow */
+  el.style.animation = null; 
+}
